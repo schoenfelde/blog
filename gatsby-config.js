a310e48fd53f9062923a51d9dee3bf404aca3b99
Elliot Schoenfeld <elliot@ollieware.com>
2020-05-19 11:55:42 +0530

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,53 +1,58 @@
 module.exports = {
   siteMetadata: {
-    title: `Catalyst`,
-    description: `Speed up your GatsbyJS development workflow. Designed as a set of opinionated and advanced themes and starters using MDX and Theme-UI. Incorporates gatsby-theme-catalyst-core, gatsby-theme-catalyst-header-top, and gatsby-theme-catalyst-footer.`,
-    keywords: [`gatsby`, `theme`, `react`],
-    author: `Eric Howey`,
-    siteUrl: `https://gatsby-starter-catalyst.netlify.app`, //Change to you site address, required for sitemap.xml and robots.txt file among other things
+    title: `Elliot Schoenfeld`,
+    description: `I am a person who wants to be a better story teller and writer, so I will be putting my thoughts and stories here.`,
+    keywords: [`writer`, `traveller`, `engineer`, `travel`],
+    author: `Elliot Schoenfeld`,
+    siteUrl: `https://pizzadevs.com`, //Change to you site address, required for sitemap.xml and robots.txt file among other things
     menuLinks: [
       {
-        name: `Page 1`,
-        link: `/page-1`,
+        name: `About`,
+        link: `/about`,
         type: `internal`, //internal or anchor
       },
-      {
-        name: `Anchor 1`,
-        link: `#anchor-1`,
-        type: `anchor`, //internal or anchor
-      },
-      {
-        name: `Page 2`,
-        link: `/page-2`,
-        type: `internal`, //internal or anchor
-        subMenu: [
-          {
-            name: `Sub 1`,
-            link: `/sub-1`,
-            type: `internal`, //internal or anchor
-          },
-          {
-            name: `Sub 2`,
-            link: `/sub-2`,
-            type: `internal`, //internal or anchor
-          },
-        ],
-      },
+      //      {
+      //        name: `Anchor 1`,
+      //        link: `#anchor-1`,
+      //        type: `anchor`, //internal or anchor
+      //      },
+      //      {
+      //        name: `Page 2`,
+      //        link: `/page-2`,
+      //        type: `internal`, //internal or anchor
+      //        subMenu: [
+      //          {
+      //            name: `Sub 1`,
+      //            link: `/sub-1`,
+      //            type: `internal`, //internal or anchor
+      //          },
+      //          {
+      //            name: `Sub 2`,
+      //            link: `/sub-2`,
+      //            type: `internal`, //internal or anchor
+      //          },
+      //        ],
+      //      },
     ],
     socialLinks: [
       {
         name: `Email`,
-        link: `eric@erichowey.dev`,
+        link: `elliot@ollieware.com`,
         location: `footer`, //Options are "all", "header", "footer"
       },
       {
-        name: `Twitter`,
-        link: `https://twitter.com/erchwy`,
-        location: `header`, //Options are "all", "header", "footer"
+        name: `Youtube`,
+        link: `https://youtube.com/channel/UCeDS7zMtJG_b-vkUiENUQig`,
+        location: `all`, //Options are "all", "header", "footer"
+      },
+      {
+        name: `Instagram`,
+        link: `https://www.instagram.com/schoenfelde`,
+        location: `all`, //Options are "all", "header", "footer"
       },
       {
         name: `Github`,
-        link: `https://www.github.com/ehowey`,
+        link: `https://www.github.com/schoenfelde`,
         location: `all`, //Options are "all", "header", "footer"
       },
     ],
@@ -71,7 +76,7 @@ module.exports = {
         // remarkImagesWidth: 1440,
       },
     },
-    `gatsby-theme-catalyst-header-top`, // Try `gatsby-theme-catalyst-header-side`
+    `gatsby-theme-catalyst-header-side`, // Try `gatsby-theme-catalyst-header-side`
     `gatsby-theme-catalyst-footer`,
     {
       resolve: `gatsby-plugin-manifest`,
